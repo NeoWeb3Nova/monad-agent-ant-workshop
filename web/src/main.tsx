c8966f56acc9103a42476f41e480fd9b9c7dd9c8
Neo.Yun <221855057+NeoWeb3Nova@users.noreply.github.com>
2026-07-25 16:03:01 +0800

    feat(web): add colony diorama and ant visual assets

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import './visual-assets.css'
 import { Providers } from './Providers.tsx'
 
 createRoot(document.getElementById('root')!).render(
